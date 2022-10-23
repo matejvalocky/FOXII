853c3034da72c1ac204cb7fa75dd2b4e793c8232
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2022-10-23 23:32:39 +0200

    pridanie javascript kodu pre automatické posúvanie DIVOV, pridanie obecného CSS a HTML

--- scripts.js
+++ scripts.js
@@ -64,3 +64,19 @@
 
 
 
+// AUTOMATIC SCROLLED DIVS
+
+
+function Divs() {
+	var divs= $('#parent div'),
+		now = divs.filter(':visible'),
+		next = now.next().length ? now.next() : divs.first(),
+		speed = 1000;
+
+	now.fadeOut(speed);
+	next.fadeIn(speed);
+}
+
+$(function () {
+	setInterval(Divs, 1400);
+});
